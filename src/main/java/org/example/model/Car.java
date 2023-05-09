6332731c784d66e1dc61286ce6519b309df1f1cf
Erick Romero <erjhrome@bancolombia.com.co>
2023-05-09 18:52:14 -0500

    add car model

---
+++ src/main/java/org/example/model/Car.java
@@ -0,0 +1,14 @@
+package org.example.model;
+
+public class Car {
+
+    private String id;
+    private String name;
+    private String typeOfCar;
+
+    private String cubitCapacity;
+
+    private String color;
+
+    private String size;
+}
